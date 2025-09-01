0d3e69fd4743f955eb3231d877817a06506b0d47
davit <davidgyulinyan@gmail.com>
2025-09-01 23:09:13 +0400

    ci: setup the store/slices

---
+++ src/store/slices/garageSlice.ts
@@ -0,0 +1,47 @@
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
+import { Car } from '../../types';
+import { garageApi } from '../../services/api';
+
+interface GarageState {
+  cars: Car[];
+  currentPage: number;
+  totalCount: number;
+  selectedCar: Car | null;
+}
+
+const initialState: GarageState = {
+  cars: [],
+  currentPage: 1,
+  totalCount: 0,
+  selectedCar: null,
+};
+
+export const fetchCars = createAsyncThunk(
+  'garage/fetchCars',
+  async (page: number) => {
+    const response = await garageApi.getCars(page);
+    return { cars: response, totalCount: 100 };
+  }
+);
+
+const garageSlice = createSlice({
+  name: 'garage',
+  initialState,
+  reducers: {
+    setPage: (state, action: PayloadAction<number>) => {
+      state.currentPage = action.payload;
+    },
+    selectCar: (state, action: PayloadAction<Car>) => {
+      state.selectedCar = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchCars.fulfilled, (state, action) => {
+      state.cars = action.payload.cars;
+      state.totalCount = action.payload.totalCount;
+    });
+  },
+});
+
+export const { setPage, selectCar } = garageSlice.actions;
+export default garageSlice.reducer;
